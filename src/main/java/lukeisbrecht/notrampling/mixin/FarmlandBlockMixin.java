d2600b6f977d0a993939b900e6eb0d9863abdc96
lukeisbrecht <lukeisbrecht25@gmail.com>
2025-12-29 23:36:18 -0500

    Added Basic Functionality

---
+++ src/main/java/lukeisbrecht/notrampling/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,20 @@
+package lukeisbrecht.notrampling.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin {
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    private void preventTrample(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
+        ci.cancel();
+        entity.handleFallDamage(fallDistance, 1.0F, world.getDamageSources().fall());
+    }
+}
