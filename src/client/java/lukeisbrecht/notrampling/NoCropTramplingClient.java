bbd90501f87ea01d5ceefb2388122559ff064315
lukeisbrecht <lukeisbrecht25@gmail.com>
2025-12-29 23:22:50 -0500

    Initial Commit

---
+++ src/client/java/lukeisbrecht/notrampling/NoCropTramplingClient.java
@@ -0,0 +1,10 @@
+package lukeisbrecht.notrampling;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoCropTramplingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/lukeisbrecht/notrampling/NoCropTramplingDataGenerator.java
@@ -0,0 +1,11 @@
+package lukeisbrecht.notrampling;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NoCropTramplingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
